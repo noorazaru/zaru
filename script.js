f764599b156a142f96dc1ca395cccb6e472aed33
noorazaru <noora.varonen@gmail.com>
2024-11-18 21:47:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 function toggleMenu() {
-    const menu = document.querySelector('.side-menu');
-    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+    const menu = document.getElementById("menu");
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 }
-
-document.addEventListener('scroll', function () {
-    const title = document.querySelector('.site-title');
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    title.classList.toggle('show', scrollTop > 100);
-});
