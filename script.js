7e6e693a40e11708ad26234e1b959d7915e70085
noorazaru <noora.varonen@gmail.com>
2024-11-18 22:58:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 function toggleMenu() {
     const menu = document.getElementById("menu");
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-    }
+    menu.classList.toggle("show");
 }
